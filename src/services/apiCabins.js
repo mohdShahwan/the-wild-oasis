c3f0a027774c4018eb2d0dc8af06e92a557d2426
mohdShahwan <mshahwan002@gmail.com>
2024-01-10 20:24:52 +0300

    Show cabins + delete

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,9 +4,20 @@ export async function getCabins() {
   let { data: cabins, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.log(error);
+    console.error(error);
     throw new Error("Cabins could not be loaded");
   }
 
   return cabins;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+
+  return data;
 }
