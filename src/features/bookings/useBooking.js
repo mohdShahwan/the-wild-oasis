f5fd67e08335805d5fdf4d6e421ed07adaf0e27b
mohdShahwan <mshahwan002@gmail.com>
2024-01-16 15:03:09 +0300

    Implemented single booking page

---
+++ src/features/bookings/useBooking.js
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import { getBooking } from "../../services/apiBookings";
+import { useParams } from "react-router-dom";
+
+export function useBooking() {
+  const { bookingId } = useParams();
+
+  const {
+    isLoading,
+    data: booking,
+    error,
+  } = useQuery({
+    queryKey: ["booking", bookingId],
+    queryFn: () => getBooking(bookingId),
+    // If the booking doesn't exist, then react query will
+    // retry to fetch data as the response will be an error
+    retry: false,
+  });
+
+  return { isLoading, booking, error };
+}
