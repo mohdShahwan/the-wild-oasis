2f039159f6f46b83540e6ab8ee0f8a8103612c35
mohdShahwan <mshahwan002@gmail.com>
2024-01-18 22:39:01 +0300

    Bug fixes

--- src/ui/Pagination.jsx
+++ src/ui/Pagination.jsx
@@ -84,13 +84,13 @@ function Pagination({ count }) {
 
   return (
     <StyledPagination>
-      <p>
+      <P>
         Showing <span>{(currentPage - 1) * PAGE_SIZE + 1}</span> to{" "}
         <span>
           {currentPage === pageCount ? count : currentPage * PAGE_SIZE}
         </span>{" "}
         of <span>{count}</span> results
-      </p>
+      </P>
 
       <Buttons>
         <PaginationButton onClick={previousPage}>
