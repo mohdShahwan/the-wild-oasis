109aa3ffd8cd535c83e8bc0d87368d9db6cd3410
mohdShahwan <mshahwan002@gmail.com>
2024-01-18 18:27:56 +0300

    Building recent bookings and stays

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,13 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
